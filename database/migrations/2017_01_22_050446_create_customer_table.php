0f307cdce1d2b0c93ed737a1486a3ca424f508a5
cuoool <chea.bonnak@gmail.com>
2017-01-22 12:14:10 +0700

    Create customer table

---
+++ database/migrations/2017_01_22_050446_create_customer_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table)
+        {
+            $table->increments('id');
+            $table->string('surname');
+            $table->string('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
