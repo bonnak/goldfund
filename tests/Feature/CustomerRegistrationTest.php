43ca1a238af408e868a63b7cb11bc42e8dc129ff
Bonnak <chea.bonnak@gmail.com>
2017-01-28 21:34:02 +0700

    Fix loading test

---
+++ tests/Feature/CustomerRegistrationTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class CustomerRegistrationTest extends TestCase
+{
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
