01a9958cf13d462b245421692ba7d725ece2d8ad
Bonnak <chea.bonnak@gmail.com>
2017-02-01 22:46:25 +0700

    Update user model and factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -15,9 +15,11 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
     static $password;
 
     return [
-        'name' => $faker->name,
+        'username' => $faker->username,
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
+        'type' => 'admin',
+        'is_active' => true,
         'remember_token' => str_random(10),
     ];
 });
