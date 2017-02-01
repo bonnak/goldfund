41f9ecbd1bdd340c56941777a8a089dede7b25ad
Bonnak <chea.bonnak@gmail.com>
2017-02-01 22:45:42 +0700

    Install tinker package

--- config/app.php
+++ config/app.php
@@ -179,6 +179,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Laravel\Passport\PassportServiceProvider::class,
+        Laravel\Tinker\TinkerServiceProvider::class,
 
     ],
 
