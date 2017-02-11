ad4ac981de348f999fbc995ca9205877786fec60
Bonnak <chea.bonnak@gmail.com>
2017-02-12 00:13:38 +0700

    No need to write bcrypt every where

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -10,7 +10,7 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
         'username' => $faker->username,
         'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
+        'password' => $password ?: $password = 'secret',
         'is_active' => true,
         'remember_token' => str_random(10),
     ];
@@ -21,7 +21,7 @@ $factory->define(App\Customer::class, function (Faker\Generator $faker) {
     return [
         'username' => $faker->username,
         'email' => $faker->unique()->safeEmail,
-        'password' => bcrypt('12345678'),
+        'password' => '12345678',
         'country_id' => $faker->numberBetween(1, 20),
         'is_active' => true,
         'remember_token' => str_random(10),
