5c9800b2788106a9c700fce5e36fb3f7e1785f54
Bonnak <chea.bonnak@gmail.com>
2017-02-17 00:01:24 +0700

    Fix style in mobile version and remove app.js of vue

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -148,7 +148,6 @@
     <script src="/js-template/smoothscroll.js"></script>
     <script src="/js-template/jquery.vegas.min.js"></script>
     <script src="/js-template/zerif.js"></script>
-    <script src="/js/app.js"></script>
 
     <script>
         $('.date').datepicker({
