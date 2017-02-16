77863c50b33c9716c62d3d9ac67b9480cffe6133
Bonnak <chea.bonnak@gmail.com>
2017-02-16 13:55:46 +0700

    Send only one mail after register successful

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'Gold Fund Trading',
+    'name' => 'Bit Company Trading',
 
     /*
     |--------------------------------------------------------------------------
