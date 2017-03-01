656555053719de17c411bba988c87d7c234fb6ca
nak2bo <nak2bo@yahoo.com>
2017-03-01 15:08:52 +0700

    Fix error auth:api and front-end deposit

--- public/js/controllers/DepositController.js
+++ public/js/controllers/DepositController.js
@@ -5,7 +5,7 @@ angular.module('MetronicApp').controller('DepositController', [
     function($scope, $anchorScroll, Restful) {
         var vm = this;
         vm.model = {};
-        console.log(vm.model);
+        vm.plans = [];
 
         vm.save = function(){
             if (!$scope.depositForm.$valid) {
@@ -17,10 +17,18 @@ angular.module('MetronicApp').controller('DepositController', [
         vm.getQrCode = function(params){
             Restful.get('api/qr/admin/bitcoin').success(function(data){
                 vm.qrcode = data;
-                console.log(data);
             });
         };
+
+        vm.getPlans = function(){
+            Restful.get('/api/plans').success(function(data){
+                data.forEach(function(el){
+                    vm.plans.push(el);
+                });
+            });
+        }
         vm.getQrCode();
+        vm.getPlans();
         $scope.$on('$viewContentLoaded', function() {});
 
     }
