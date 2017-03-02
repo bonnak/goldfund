74fb692199bcc8a4b067793efe6ec78793be5ac4
nak2bo <nak2bo@yahoo.com>
2017-03-02 11:34:31 +0700

    Deposit history

--- public/js/controllers/DepositController.js
+++ public/js/controllers/DepositController.js
@@ -1,14 +1,16 @@
 angular.module('MetronicApp').controller('DepositController', [
     '$scope',
     '$anchorScroll',
+    '$state',
     'Restful',
-    function($scope, $anchorScroll, Restful) {
+    function($scope, $anchorScroll, $state, Restful) {
         var vm = this;
         vm.model = {
             plan_id: null,
             amount: null
         };
         vm.plans = [];
+        vm.deposits = [];
 
         vm.save = function(){
             if (!$scope.depositForm.$valid) {
@@ -18,10 +20,20 @@ angular.module('MetronicApp').controller('DepositController', [
             vm.loading = true;
 
             Restful.save('api/deposit', vm.model).success(function(data){
+                $state.go('deposit/history');
             }).finally(function () {
                 vm.loading= false;
             });;
         };
+
+        vm.getHistory = function(params){
+            Restful.get('api/deposit/history').success(function(data){
+                data.forEach(function(el){
+                    vm.deposits.push(el);
+                });
+            });
+        };
+
         vm.getQrCode = function(params){
             Restful.get('api/qr/admin/bitcoin').success(function(data){
                 vm.qrcode = data;
@@ -37,6 +49,7 @@ angular.module('MetronicApp').controller('DepositController', [
         }
         vm.getQrCode();
         vm.getPlans();
+        vm.getHistory();
         $scope.$on('$viewContentLoaded', function() {});
 
     }
