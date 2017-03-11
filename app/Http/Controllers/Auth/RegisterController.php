782d57d1a49b7cb828a89fd9ae7f0b5ab645aebb
Bonnak <chea.bonnak@gmail.com>
2017-03-11 14:16:53 +0700

    Show sponsor earning data

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -44,7 +44,7 @@ class RegisterController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest');
+        //$this->middleware('guest');
     }
 
     public function register(Request $request)
