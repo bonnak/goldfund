b51f9e39d483d13ccb05a53d6e9ac34d6cd738e4
Bonnak <chea.bonnak@gmail.com>
2017-03-12 22:21:12 +0700

    Sponsor deposit need to be active in order to receive binary pairing commission

--- tests/Feature/BinaryPairEarningTest.php
+++ tests/Feature/BinaryPairEarningTest.php
@@ -74,4 +74,170 @@ class BinaryPairEarningTest extends TestCase
         ]);
 
     }
+
+    /**
+     * @test
+     */
+    public function it_not_receive_binary_pair_earning_because_only_either_left_nor_right_child_deposited()
+    {
+    	$parent = factory(Customer::class)->create([
+            'sponsor_id' => $this->cust_admin->id,
+            'placement_id' => $this->cust_admin->id,
+            'direction' => 'R',
+        ]);
+        $parent_deposit = factory(Deposit::class)->create([ 
+            'cust_id' => $parent->id, 
+            'plan_id' => $this->plan->id, 
+            'amount' => 100,
+            'status' => 1,
+        ]);
+
+
+        $child_l = factory(Customer::class)->create([
+            'sponsor_id' => $parent->id,
+            'placement_id' => $parent->id,
+            'direction' => 'L',
+        ]);
+
+
+
+        $child_r = factory(Customer::class)->create([
+            'sponsor_id' => $parent->id,
+            'placement_id' => $parent->id,
+            'direction' => 'R',
+        ]);
+        $child_r_deposit = factory(Deposit::class)->create([ 
+            'cust_id' => $child_r->id, 
+            'plan_id' => $this->plan->id, 
+            'amount' => 100,
+        ]);
+
+
+
+        $response = $this->actingAs($this->backend_admin, 'api_admin')
+                          ->post('api/admin/deposit/' . $child_r_deposit->id . '/approve');
+
+
+
+        $this->assertDatabaseMissing('binary_earning_commissions', [
+            'cust_id'			=> $parent->id,
+            'left_child_id'		=> $child_l->id,
+            'right_child_id'	=> $child_r->id
+        ]);
+
+    }
+
+    /**
+     * @test
+     */
+    public function it_not_receive_binary_pair_earning_because_only_either_left_nor_right_child_deposit_is_approved()
+    {
+    	$parent = factory(Customer::class)->create([
+            'sponsor_id' => $this->cust_admin->id,
+            'placement_id' => $this->cust_admin->id,
+            'direction' => 'R',
+        ]);
+        $parent_deposit = factory(Deposit::class)->create([ 
+            'cust_id' => $parent->id, 
+            'plan_id' => $this->plan->id, 
+            'amount' => 100,
+            'status' => 1,
+        ]);
+
+
+        $child_l = factory(Customer::class)->create([
+            'sponsor_id' => $parent->id,
+            'placement_id' => $parent->id,
+            'direction' => 'L',
+        ]);
+        $child_l_deposit = factory(Deposit::class)->create([ 
+            'cust_id' => $child_l->id, 
+            'plan_id' => $this->plan->id, 
+            'amount' => 80,
+            'status' => 0,
+        ]);
+
+
+        $child_r = factory(Customer::class)->create([
+            'sponsor_id' => $parent->id,
+            'placement_id' => $parent->id,
+            'direction' => 'R',
+        ]);
+        $child_r_deposit = factory(Deposit::class)->create([ 
+            'cust_id' => $child_r->id, 
+            'plan_id' => $this->plan->id, 
+            'amount' => 100,
+        ]);
+
+
+
+        $response = $this->actingAs($this->backend_admin, 'api_admin')
+                          ->post('api/admin/deposit/' . $child_r_deposit->id . '/approve');
+
+
+
+        $this->assertDatabaseMissing('binary_earning_commissions', [
+            'cust_id'			=> $parent->id,
+            'left_child_id'		=> $child_l->id,
+            'right_child_id'	=> $child_r->id
+        ]);
+
+    }
+
+
+    /**
+     * @test
+     */
+    public function sponsor_deposit_must_be_active_in_order_to_receive_binary_pair_earning()
+    {
+    	$parent = factory(Customer::class)->create([
+            'sponsor_id' => $this->cust_admin->id,
+            'placement_id' => $this->cust_admin->id,
+            'direction' => 'R',
+        ]);
+        $parent_deposit = factory(Deposit::class)->create([ 
+            'cust_id' => $parent->id, 
+            'plan_id' => $this->plan->id, 
+            'amount' => 100,
+            'status' => 0,
+        ]);
+
+
+        $child_l = factory(Customer::class)->create([
+            'sponsor_id' => $parent->id,
+            'placement_id' => $parent->id,
+            'direction' => 'L',
+        ]);
+        $child_l_deposit = factory(Deposit::class)->create([ 
+            'cust_id' => $child_l->id, 
+            'plan_id' => $this->plan->id, 
+            'amount' => 80,
+            'status' => 1,
+        ]);
+
+        $child_r = factory(Customer::class)->create([
+            'sponsor_id' => $parent->id,
+            'placement_id' => $parent->id,
+            'direction' => 'R',
+        ]);
+        $child_r_deposit = factory(Deposit::class)->create([ 
+            'cust_id' => $child_r->id, 
+            'plan_id' => $this->plan->id, 
+            'amount' => 100,
+        ]);
+
+
+
+        $response = $this->actingAs($this->backend_admin, 'api_admin')
+                          ->post('api/admin/deposit/' . $child_r_deposit->id . '/approve');
+
+
+
+        $this->assertDatabaseMissing('binary_earning_commissions', [
+            'cust_id'			=> $parent->id,
+            'left_child_id'		=> $child_l->id,
+            'right_child_id'	=> $child_r->id
+        ]);
+
+    }
 }
