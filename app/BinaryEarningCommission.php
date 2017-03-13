cd56b6782be290a03087345cb6abc3c102185d87
Bonnak <chea.bonnak@gmail.com>
2017-03-13 22:49:57 +0700

    View dashboard transaction

--- app/BinaryEarningCommission.php
+++ app/BinaryEarningCommission.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Customer;
 
 class BinaryEarningCommission extends Model
 {
@@ -11,4 +12,19 @@ class BinaryEarningCommission extends Model
     protected $fillable = [
     	'cust_id', 'left_child_id', 'right_child_id', 'amount',
     ];
+
+    public function sponsor()
+    {
+    	return $this->belongsTo(Customer::class, 'cust_id', 'id');
+    }
+
+    public function left_child()
+    {
+    	return $this->belongsTo(Customer::class, 'left_child_id', 'id');
+    }
+
+    public function right_child()
+    {
+    	return $this->belongsTo(Customer::class, 'right_child_id', 'id');
+    }
 }
