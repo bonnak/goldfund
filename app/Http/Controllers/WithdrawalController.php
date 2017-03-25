934011d32174b4ab880d1b1488c8185ec760723f
Bonnak <chea.bonnak@gmail.com>
2017-03-25 16:51:11 +0700

    Fix withdrawal and dashboard

--- app/Http/Controllers/WithdrawalController.php
+++ app/Http/Controllers/WithdrawalController.php
@@ -8,6 +8,7 @@ use App\Earning;
 use App\SponsorEarningCommission;
 use App\BinaryEarningCommission;
 use App\Withdrawal;
+use App\LevelEarningCommission;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class WithdrawalController extends Controller
@@ -20,15 +21,18 @@ class WithdrawalController extends Controller
         $sponsor = SponsorEarningCommission::where('sponsor_id', auth()->user()->id)
                             ->where('status', 1)
                             ->sum('amount');
+        $level = LevelEarningCommission::where('cust_id', auth()->user()->id)
+                            ->where('status', 1)
+                            ->sum('amount');
         $binary = BinaryEarningCommission::where('cust_id', auth()->user()->id)
                             ->where('status', 1)
                             ->sum('amount');
         $withdrawal = Withdrawal::where('cust_id', auth()->user()->id)
-                            ->where('status', 1)
+                            //->where('status', 1)
                             ->sum('amount');
 
 
-    	return ($earning + $sponsor + $binary) - $withdrawal; 
+    	return ($earning + $sponsor + $binary + $level) - $withdrawal; 
     }
 
     public function cashOut(Request $request)
@@ -36,6 +40,12 @@ class WithdrawalController extends Controller
         if(auth()->user()->is_active != 1) 
         {
             throw new HttpException(403, 'Your account is inactive or expiry.');
+        }
+
+        $today_withdrawals = auth()->user()->withdrawals->where('created_at', '>=', Carbon::today());
+        if(!$today_withdrawals->isEmpty())
+        {
+            throw new HttpException(403, 'Allow to withdrawal only once per day');
         }
 
 
@@ -53,6 +63,7 @@ class WithdrawalController extends Controller
     	return Withdrawal::create([
     		'cust_id'	=> auth()->user()->id,
     		'amount'	=> $request->withdraw_amount,
+            'status'    => 0,
     	]);
     }
 
