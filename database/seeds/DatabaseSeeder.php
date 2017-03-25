4b09cafd013e2fc0b2c8b2e1fc040f33b4f3156c
Bonnak <chea.bonnak@gmail.com>
2017-03-25 18:50:10 +0700

    Update seed for production

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,9 +15,7 @@ class DatabaseSeeder extends Seeder
     $this->call(CompanyProfileSeeder::class);
     $this->call(UsersTableSeeder::class);
     $this->call(PlansTableSeeder::class);
-    $this->call(CustomersTableSeeder::class);
     $this->call(ServicesTableSeeder::class);
-    //$this->call(DepositsTableSeeder::class);
     $this->call(FaqTableSeeder::class);
   }
 }
