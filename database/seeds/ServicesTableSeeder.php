2ad5f89b00c6b0fcedd5d8e0f6955b98f6840653
HuySokhom <sokhomhuy.corarl@gmail.com>
2017-03-25 16:54:14 +0700

    update seed with company name in service

--- database/seeds/ServicesTableSeeder.php
+++ database/seeds/ServicesTableSeeder.php
@@ -26,15 +26,15 @@ class ServicesTableSeeder extends Seeder
                 recorded in Bitcon\'s public ledger, the block chain. By conducting a random lottery
                 where electricity and specialized equipment are the price of admission, the cost to
                 disrupt the Bitcoin network scales with the amount of hashing power that is being spent
-                by all mining participants.</p><p style="font-size:24px; font-weight:bold">What is GB-Forex Mining</p>
-                <p style="text-align:justify">GB-Forex Enter in Bitcoin Mining at the end of 2014. They got to know
-                each other by using the same platform for buying and selling Bitcoins. GB-Forex company
+                by all mining participants.</p><p style="font-size:24px; font-weight:bold">What is Bit Company Trading Mining</p>
+                <p style="text-align:justify">Bit Company Trading Enter in Bitcoin Mining at the end of 2014. They got to know
+                each other by using the same platform for buying and selling Bitcoins. Bit Company Trading company
                 and its user base grew, new mining farms were built up and several additional people
-                hired, specifically programmers and engineers. The current members of GB-Forex come from
+                hired, specifically programmers and engineers. The current members of Bit Company Trading come from
                 different scientific disciplines, but our common faith in cryptocurrencies has brought
-                us together.GB-Forex strong believers in the future of digital currencies and GB-Forex
-                love being part of this growing community! GB-Forex having large number of bitcoin Machine
-                on it own farm. GB-Forex Share his Profits to the investor.</p>'
+                us together.Bit Company Trading strong believers in the future of digital currencies and Bit Company Trading
+                love being part of this growing community! Bit Company Trading having large number of bitcoin Machine
+                on it own farm. Bit Company Trading Share his Profits to the investor.</p>'
             ],
             [
                 'title' => 'Forex Trading',
@@ -49,23 +49,23 @@ class ServicesTableSeeder extends Seeder
                     location or central exchange and trades 24-hours a day through a global network of
                     businesses, banks and individuals. This means that currency prices are constantly
                     fluctuating in value against each other, offering multiple trading opportunities.</p>
-                    <p style="font-size:24px; font-weight:bold">What is GB-Forex Trading</p>
+                    <p style="font-size:24px; font-weight:bold">What is Bit Company Trading Trading</p>
                      <p style="text-align:justify">
-                     GB-Forex Trade Responsibly: GB-Forex are complex financial products that are traded
-                     on margin. Trading GB-Forex carries a high level of risk since leverage can work both
-                     to your advantage and disadvantage. As a result, GB-Forex may suitable for all
-                     investors because you may not lose all your invested capital. GB-Forex should not
-                     risk more than you are prepared to lose. Before deciding to trade, GB-Forex need to
+                     Bit Company Trading Trade Responsibly: Bit Company Trading are complex financial products that are traded
+                     on margin. Trading Bit Company Trading carries a high level of risk since leverage can work both
+                     to your advantage and disadvantage. As a result, Bit Company Trading may suitable for all
+                     investors because you may not lose all your invested capital. Bit Company Trading should not
+                     risk more than you are prepared to lose. Before deciding to trade, Bit Company Trading need to
                      ensure that the risks involved taking into account your investment objectives and
-                     level of experience. Past performance of GB-Forex is a reliable indicator of future
-                     results. Most GB-Forex have no set maturity date. Hence, a GB-Forex position matures
+                     level of experience. Past performance of Bit Company Trading is a reliable indicator of future
+                     results. Most Bit Company Trading have no set maturity date. Hence, a Bit Company Trading position matures
                      on the date you choose to close an existing open position. Seek independent advice,
-                     if necessary. Please read GB-Forex full ‘Risk Disclosure Statement’.</p>
+                     if necessary. Please read Bit Company Trading full ‘Risk Disclosure Statement’.</p>
                      <p style="text-align:justify">A relative newcomer to the world of charting platforms,
                      cTrader has already developed a loyal following among traders looking for an added
-                     level of market resolution. Designed specifically for the trading of GB-Forex,
-                     GB-Forex is a trading platform allowing traders to place orders while having access
-                     to full market depth. GB-Forex provides its traders on the GB-Forex trading platform
+                     level of market resolution. Designed specifically for the trading of Bit Company Trading,
+                     Bit Company Trading is a trading platform allowing traders to place orders while having access
+                     to full market depth. Bit Company Trading provides its traders on the Bit Company Trading trading platform
                      with the best available bid and ask prices, even when they come from competing
                      institutions. We also fill orders at VWAP (Volume-Weighted Average Price),
                      executing through the available liquidity tiers until the order is filled.
@@ -89,23 +89,23 @@ class ServicesTableSeeder extends Seeder
                     on the intra-day spot price, derived from over-the-counter gold-trading markets around
                     the world (code "XAU"). The following table sets forth the gold price versus various
                     assets and key statistics on the basis of data taken with the frequency of five years.</p>
-                    <p style="font-size:24px; font-weight:bold">What is GB-Forex Gold Trading</p>
-                    <p style="text-align:justify"> GB-Forex Trade Responsibly: GB-Forex are complex
-                    financial products that are traded on margin. Trading GB-Forex carries a high level
+                    <p style="font-size:24px; font-weight:bold">What is Bit Company Trading Gold Trading</p>
+                    <p style="text-align:justify"> Bit Company Trading Trade Responsibly: Bit Company Trading are complex
+                    financial products that are traded on margin. Trading Bit Company Trading carries a high level
                     of risk since leverage can work both to your advantage and disadvantage. As a result,
-                    GB-Forex may suitable for all investors because you may not lose all your invested
-                    capital. GB-Forex should not risk more than you are prepared to lose. Before deciding
-                    to trade, GB-Forex need to ensure that the risks involved taking into account your
-                    investment objectives and level of experience. Past performance of GB-Forex is a
-                    reliable indicator of future results. Most GB-Forex have no set maturity date.
-                    Hence, a GB-Forex position matures on the date you choose to close an existing
-                    open position. Seek independent advice, if necessary. Please read GB-Forex full
+                    Bit Company Trading may suitable for all investors because you may not lose all your invested
+                    capital. Bit Company Trading should not risk more than you are prepared to lose. Before deciding
+                    to trade, Bit Company Trading need to ensure that the risks involved taking into account your
+                    investment objectives and level of experience. Past performance of Bit Company Trading is a
+                    reliable indicator of future results. Most Bit Company Trading have no set maturity date.
+                    Hence, a Bit Company Trading position matures on the date you choose to close an existing
+                    open position. Seek independent advice, if necessary. Please read Bit Company Trading full
                     ‘Risk Disclosure Statement’.</p>
                     <p style="text-align:justify">A relative newcomer to the world of charting platforms,
                     cTrader has already developed a loyal following among traders looking for an added
-                    level of market resolution. Designed specifically for the trading of GB-Forex,
-                    GB-Forex is a trading platform allowing traders to place orders while having access
-                    to full market depth. GB-Forex provides its traders on the GB-Forex trading
+                    level of market resolution. Designed specifically for the trading of Bit Company Trading,
+                    Bit Company Trading is a trading platform allowing traders to place orders while having access
+                    to full market depth. Bit Company Trading provides its traders on the Bit Company Trading trading
                     platform with the best available bid and ask prices, even when they come from
                     competing institutions. We also fill orders at VWAP (Volume-Weighted Average Price),
                     executing through the available liquidity tiers until the order is filled.
