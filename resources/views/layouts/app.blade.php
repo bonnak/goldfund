119b78490c23d1a50f401cf770fac44088fd5331
Bonnak <chea.bonnak@gmail.com>
2017-04-04 18:21:44 +0700

    Add alexa range script

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,6 +49,14 @@
            'csrfToken' => csrf_token(),
        ]) !!};
      </script>
+     
+     <!-- Start Alexa Certify Javascript -->
+    <script type="text/javascript">
+        _atrk_opts = { atrk_acct:"dZTUo1IWx810Ug", domain:"bitcompanytrading.com",dynamic: true};
+        (function() { var as = document.createElement('script'); as.type = 'text/javascript'; as.async = true; as.src = "https://d31qbv1cthcecs.cloudfront.net/atrk.js"; var s = document.getElementsByTagName('script')[0];s.parentNode.insertBefore(as, s); })();
+    </script>
+    <noscript><img src="https://d5nxst8fruw4z.cloudfront.net/atrk.gif?account=dZTUo1IWx810Ug" style="display:none" height="1" width="1" alt="" /></noscript>
+    <!-- End Alexa Certify Javascript -->
 </head>
 <body>
     <div id="app">
